73a3220cb04aafc8d757131171c926deb0db5f44
jigudeong_i <wlrnejddl@naver.com>
2025-08-07 20:00:45 +0900

    no message

--- example-react/src/App.js
+++ example-react/src/App.js
@@ -1,39 +1,60 @@
 
-//import './App.css';    
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 import './App.css';     // ./  는 현재경로라는 뜻. 
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-  );
-};
+// 함수 컴포넌트
+// function App(){
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Body />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// 화살표 함수 컴포넌트
+
+
+  const App = ()=>{     // const App = function(){
+    return (
+      <div className="App">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    );
+  };
+
+  const ka = () => {
+      const number = 8;
+      if (number % 2 === 0){
+          return <div>{number}은(는) 짝수입니다.</div>
+      } else {
+          return <div>{number}은(는) 홀수입니다.</div>
+      }
+  };
+
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- example-react/src/components/Body.js
+++ example-react/src/components/Body.js
@@ -1,24 +1,41 @@
-const Body = () => {
+
+const chanung = () => {
     
-    const name = "박지원"
-    const address = "서울시 장안동"
-    const pn = "01023123123"
-    
+    const Body = () => {
+        
+        const name = "박지원"
+        const address = "서울시 장안동"
+        const pn = "01023123123"
+        
+        return(
+            <>
+            <div
+                className="react" //시작태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성할 수 있다.
+            ></div>      
+            {/*주석문 명시*/}
+            <h3>{name}</h3>
+            <h3>{address}</h3> 
+            <h3>{pn}</h3>
+            </>
+        )
+
+    };
+
+    const Ka = () => {
+        const number = 8;
+        if (number % 2 === 0){
+            return <div>{number}은(는) 짝수입니다.</div>
+        } else {
+            return <div>{number}은(는) 홀수입니다.</div>
+        }
+    };
+
     return(
-        <>
-        <div
-            className="react"//시작태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성할 수 있다.
-        ></div>      
-        {/*주석문 명시*/}
-        <h3>{name}</h3>
-        <h3>{address}</h3> 
-        <h3>{pn}</h3>
-        </>
-    )
+        <div>
+            <Body />
+            <Ka />
+        </div>
+    );
+}
+export default chanung;
 
-};
-
-export default Body;
-
-
-return 안은 jss. 주석 다르게 줘야함 
